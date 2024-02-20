38ae6c6f490b1bdb370fd79daa2246d879674afb
Yan Kuang <YTKme@Outlook.com>
2024-02-20 14:51:35 -0800

    Mark Phase: rich progress for aioartifactory _retrieve function

--- aioartifactory/aioartifactory.py
+++ aioartifactory/aioartifactory.py
@@ -3,6 +3,11 @@ Asynchronous Input Output (AIO) Artifactory
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
+import asyncio
+from asyncio import (
+    TaskGroup
+)
+from os import PathLike
 from pathlib import Path
 from types import TracebackType
 from typing import (
@@ -11,9 +16,15 @@ from typing import (
 )
 
 import aiofiles
-import aiohttp
+from aiohttp import (
+    ClientSession,
+    ClientTimeout,
+    TCPConnector,
+)
+import rich.progress
 
 from aioartifactory.context import TeardownContextManager
+from aioartifactory import configuration
 
 
 class AIOArtifactory:
@@ -44,37 +55,67 @@ class AIOArtifactory:
         """
         return super().__new__(cls)
 
+    async def print_me(self, message: str):
+        await asyncio.sleep(1)
+        print(message)
+
     async def retrieve(
+        self,
+        source_list: list[str],
+        destination_list: list[PathLike],
+        recursive: bool = False,
+        quiet: bool = False,
+    ):
+        """Retrieve
+
+        :param source: The source (URL) path
+        :type source: str
+        :param destination: The destination path
+        :type destination: str
+        :param recursive: Whether to recursively retrieve artifact(s)
+        :param quiet: Whether to show retrieve progress
+        :type quiet: bool, optional
+        """
+        queue = asyncio.Queue()
+        # async with TaskGroup() as group:
+        #     for source in source_list:
+        #         group.create_task(self.print_me(source))
+
+    async def _retrieve(
         self,
         source: str,
         destination: str,
         quiet: bool = False,
     ):
-        """Retrieve
-
-
-        """
         transferred_size = 0
         destination_path = Path(destination).parent.resolve()
 
-        session_connector = aiohttp.TCPConnector(limit=10)
-        session_timeout = aiohttp.ClientTimeout(total=None, sock_connect=10000, sock_read=10000)
+        session_connector = TCPConnector(limit=10)
+        session_timeout = ClientTimeout(total=None, sock_read=10000, sock_connect=10000)
         async with (
-            aiohttp.ClientSession(connector=session_connector, timeout=session_timeout) as session,
+            ClientSession(connector=session_connector, timeout=session_timeout) as session,
             session.get(source, headers=self._header) as response,
             aiofiles.open(file=destination, mode='wb') as file,
         ):
-            with TeardownContextManager() as teardown:
-                def cleanup():
-                    ...
+            total = int(response.headers['Content-Length'])
+            # with TeardownContextManager() as teardown:
+            #     def cleanup():
+            #         ...
 
-                teardown.append(cleanup)
+            #     teardown.append(cleanup)
 
+            with rich.progress.Progress(
+                '[progress.percentage]{task.percentage:>3.0f}%',
+                rich.progress.BarColumn(bar_width=None),
+                rich.progress.DownloadColumn(),
+                rich.progress.TransferSpeedColumn(),
+            ) as progress:
+                download_task = progress.add_task('Download', total=total)
                 async for chunk, _ in response.content.iter_chunks():
                     await file.write(chunk)
+                    progress.update(download_task, advance=len(chunk))
 
         print('Done')
-
 
     async def __aenter__(self):
         """Asynchronous Enter
