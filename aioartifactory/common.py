992bbe60bd5973cfd390856c75c8d7e283e23d5a
Yan Kuang <YTKme@Outlook.com>
2025-03-28 09:33:23 -0700

    Update quotation

--- aioartifactory/common.py
+++ aioartifactory/common.py
@@ -15,9 +15,9 @@ from rich.table import Column
 
 
 progress = Progress(
-    TextColumn('[progress.description]{task.description}', table_column=Column(ratio=1)),
+    TextColumn("[progress.description]{task.description}", table_column=Column(ratio=1)),
     BarColumn(bar_width=60, table_column=Column(ratio=2)),
-    TextColumn('[progress.percentage]{task.percentage:>3.0f}%', table_column=Column(ratio=1)),
+    TextColumn("[progress.percentage]{task.percentage:>3.0f}%", table_column=Column(ratio=1)),
     DownloadColumn(),
     TransferSpeedColumn(),
     console=Console(),
